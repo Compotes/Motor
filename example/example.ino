fae19bb9f76cb0499c4080c0a0213fd939daeb04
Compotes <compotesrobots@gmail.com>
2015-01-29 20:42:18 +0100

    Update example.ino

--- example/example.ino
+++ example/example.ino
@@ -7,5 +7,7 @@ void setup() {
 }
 
 void loop() {
-  
+  m.idz(1, 1, 255); // motor 1, direction 1, speed 255
+  m.idzteVeDvu(1,2,1,255); // motor 1, motor 2, direction 1, speed 255
+  m.stop(1); // motor 1
 }
