2b9094114f0a33e4a7380e62b5a334c47ab8e7e3
G9mk <gregorkaretka999@gmail.com>
2015-01-30 13:19:42 +0100

    Editing example

--- example/example.ino
+++ example/example.ino
@@ -10,4 +10,5 @@ void loop() {
   m.idz(1, 1, 255); // motor 1, direction 1, speed 255
   m.idzteVeDvu(1,2,1,255); // motor 1, motor 2, direction 1, speed 255
   m.stop(1); // motor 1
+  m.kolecko(1, 255); // direction 1, speed 2 // all motors are spinning to one direction
 }
