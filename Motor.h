db140b6674a6b6c37138ff32e64ca4e28c0de57c
G9mk <gregorkaretka999@gmail.com>
2015-02-01 17:19:32 +0100

    updating stop

--- Motor.h
+++ Motor.h
@@ -1,11 +1,11 @@
 #ifndef Motor_h
 #define Motor_h
-#include <Arduino.h>
+#include "Arduino.h"
 
 class Motor {
 	public:
 		Motor();
-		void stop(int number, int mils);
+		void stop(int number, int number2 = 0, int number3 = 0, int number4 = 0);
 		void idz(int number, int dir, int speed);
 		void kolecko(int dir, int speed);
 		void idzteVeDvu(int number1, int number2, int dir, int speed);
