3624433163d504c703efa68dd5a4c049fe201c33
Compotes <compotesrobots@gmail.com>
2015-02-01 17:25:18 +0100

    Update example.ino

--- example/example.ino
+++ example/example.ino
@@ -9,6 +9,6 @@ void setup() {
 void loop() {
   m.idz(1, 1, 255); // motor 1, direction 1, speed 255
   m.idzteVeDvu(1,2,1,255); // motor 1, motor 2, direction 1, speed 255
-  m.stop(1); // motor 1
+  m.stop(1,2); // motor 1,2 // You can use it with different motors
   m.kolecko(1, 255); // direction 1, speed 2 // all motors are spinning to one direction
 }
